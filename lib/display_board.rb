b3a2fd83e922a19d5a1cb13129858c038b6f50fa
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-02-02 00:15:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@
 # out the current state.
 
 def display_board(board)
+  board = [" "," "," "," "," "," "," "," "," "]
+  
   puts " #{display_board[0]} | #{display_board[1]} | #{display_board[2]} "
   puts "-----------"
   puts " #{display_board[3]} | #{display_board[4]} | #{display_board[5]} "
