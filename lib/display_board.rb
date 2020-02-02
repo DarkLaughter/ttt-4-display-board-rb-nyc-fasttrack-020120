bf3004b7333db889e23cedc12c43e0a252cdefac
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-02-02 00:02:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(display_board)
- puts " #{display_board[0]} | #{display_board[1]}  | #{display_board[2]} "
+ puts " #{[0]} | #{[1]}  | #{[2]} "
  puts "-----------"
- puts " #{display_board[3]} | #{display_board[4]}  | #{display_board[5]} "
+ puts " #{[3]} | #{[4]}  | #{[5]} "
  puts "-----------"
- puts " #{display_board[6]} | #{display_board[7]}  | #{display_board[8]} "
+ puts " #{[6]} | #{[7]}  | #{[8]} "
 end
 
 
