151d7a8255fc13c8f6890d84f53958cf732cff93
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-02-02 00:13:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
+
+def display_board(board)
+  puts " #{display_board[0]} | #{display_board[1]} | #{display_board[2]} "
+  puts "-----------"
+  puts " #{display_board[3]} | #{display_board[4]} | #{display_board[5]} "
+  puts "-----------"
+  puts " #{display_board[6]} | #{display_board[7]} | #{display_board[8]} "
+
+end
+
+board1 = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+
+display_board(board1)
